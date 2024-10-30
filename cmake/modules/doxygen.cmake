79b4c7c1e095fdb02691f3fa9011e79781aaf815
Siyuan Huang <gz.siyuan@qq.com>
2024-10-30 02:55:05 +0100

    cmake module involving doxygen completed

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -6,7 +6,6 @@ option(BUILD_DOCUMENTATION "Build Doxygen documentation" ON)
 # TODO: Add a custom target for building the documentation.
 if (BUILD_DOCUMENTATION AND ${DOXYGEN_FOUND})
     set(DOXYGEN_CONFIG "${CMAKE_SOURCE_DIR}/Doxyfile")
-
     add_custom_target(doc_doxygen
             COMMAND ${DOXYGEN_EXECUTABLE} "${DOXYGEN_CONFIG}"
             WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
