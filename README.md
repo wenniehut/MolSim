04b93af378fa49beca7f48dfd305c2dba2a90cc6
Siyuan Huang <gz.siyuan@qq.com>
2024-10-30 01:33:41 +0100

    .

--- README.md
+++ README.md
@@ -3,9 +3,16 @@ MolSim GROUP D
 
 
 
-# build
-mkdir build & cd build & cmake ..
-# make 
-make
-# run
-run
+## Instructions
+### Build and Run
+`mkdir build`<br>
+`cd build`<br>
+`cmake .. [-DBUILD_DOCUMENTATION=ON|OFF]`<br>
+`make`<br>
+`./MolSim <path_to_input_file>`
+
+### Generate Doxygen Documentations
+`make doc_doxygen`<br>
+
+Note: Only possible if the project was built with the option
+`-DBUILD_DOCUMENTATION=ON`
--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -1,5 +1,15 @@
 # make doc_doxygen optional if someone does not have / like doxygen
 
 # TODO: create CMake build option for the target.
-
+find_package(Doxygen)
+option(BUILD_DOCUMENTATION "Build Doxygen documentation" ON)
 # TODO: Add a custom target for building the documentation.
+if (BUILD_DOCUMENTATION AND ${DOXYGEN_FOUND})
+    set(DOXYGEN_CONFIG "${CMAKE_SOURCE_DIR}/Doxyfile")
+
+    add_custom_target(doc_doxygen
+            COMMAND ${DOXYGEN_EXECUTABLE} "${DOXYGEN_CONFIG}"
+            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    )
+    set_property(TARGET doc_doxygen PROPERTY EXCLUDE_FROM_ALL TRUE)
+endif()
